6a258f48d9f3635fe3d9e64a0f82408ccde665de
felipesora <felipeusora@gmail.com>
2025-10-07 14:03:37 -0300

    primeiro commit

---
+++ src/main/java/br/com/cp5_java_advanced/Cp5JavaAdvancedApplication.java
@@ -0,0 +1,13 @@
+package br.com.cp5_java_advanced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cp5JavaAdvancedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cp5JavaAdvancedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cp5_java_advanced/Cp5JavaAdvancedApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cp5_java_advanced;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cp5JavaAdvancedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
